bab5706a245d0bb29915619580244a2273c71188
Perry-perrota <perryobara@gmail.com>
2019-02-25 12:50:21 +0300

    adds a container to the entire webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-       $("input[type='button']").click(function(event){
+ $("input[type='button']").click(function(event){
   var name1 = $("input[name='name1']").val();
   var name2 = $("input[name='name2']").val();
+  document.getElementById("nameoutput").innerHTML="This is your name"+" " + name1 +" "+name2;
+});
+ $("input[type='button']").click(function(event){
   var queone = parseInt($("input[name='num1']:checked").val());
   var quetwo = parseInt($("input[name='num2']:checked").val());
   var quethree = parseInt($("input[name='num3']:checked").val());
@@ -12,17 +15,22 @@ $(document).ready(function(){
   var queeight = parseInt($("input[name='num8']:checked").val());
   var quenine = parseInt($("input[name='num9']:checked").val());
   var queten = parseInt($("input[name='num10']:checked").val());
-  var questions=[];
+        var questions=[];
     questions.push(queone,quetwo,quethree,quefour,quefive,quesix,queseven,queeight,quenine,queten);
+    var i;
+    for (i = 0; i < questions.length; i++) {
+      if (questions[i]===isNaN()){
+        return questions[i]===0;
+      }
+      else {
+        return questions[i];
+      }
+}
     var total =0;
       questions.forEach(function(question) {
         total += question;
+        document.getElementById("scoreoutput").innerHTML="This is your score"+" " + total;
 });
-        
-
       event.preventDefault();
-
-
-
   });
 });
