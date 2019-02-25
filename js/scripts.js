617753ee9529a96ba539478a257c0bb1d08840df
Perry-perrota <perryobara@gmail.com>
2019-02-25 10:22:03 +0300

    adds values from each question to the final score

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
-
 $(document).ready(function(){
-       $("input[type='button']").click(function(){
-  var quesone = parseInt($("input[name='question1']:checked").val());
-  var questwo = parseInt($("input[name='question2']:checked").val());
-  var questhree = parseInt($("input[name='question3']:checked").val());
-  var quesfour = parseInt($("input[name='question4']:checked").val());
-  var quesfive = parseInt($("input[name='question5']:checked").val());
-  var quessix = parseInt($("input[name='question6']:checked").val());
-  var quesseven = parseInt($("input[name='question7']:checked").val());
-  var queseight = parseInt($("input[name='question8']:checked").val());
-  var quesnine = parseInt($("input[name='question9']:checked").val());
-  var questen = parseInt($("input[name='question10']:checked").val());
-           }
-       });
 
-   });
+       $("input[type='button']").click(function(event){
+  var queone = parseInt($("input[name='num1']:checked").val());
+  var quetwo = parseInt($("input[name='num2']:checked").val());
+  var quethree = parseInt($("input[name='num3']:checked").val());
+  var quefour = parseInt($("input[name='num4']:checked").val());
+  var quefive = parseInt($("input[name='num5']:checked").val());
+  var quesix = parseInt($("input[name='num6']:checked").val());
+  var queseven = parseInt($("input[name='num7']:checked").val());
+  var queeight = parseInt($("input[name='num8']:checked").val());
+  var quenine = parseInt($("input[name='num9']:checked").val());
+  var queten = parseInt($("input[name='num10']:checked").val());
+  if ($.trim($("#num1").val()) === "") {
+      alert('you did not fill out one of the fields');
+      return false;
+  var questions=[];
+    questions.push(queone,quetwo,quethree,quefour,quefive,quesix,queseven,queeight,quenine,queten);
+    alert(questions);
+      event.preventDefault();
+
+    alert(total);
+
+  });
+});
