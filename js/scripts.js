aafdca7600f4d60a4f5500313065e22cf1aa984b
Perry-perrota <perryobara@gmail.com>
2019-02-25 08:00:21 +0300

    adds the jquery function which will receive input from the html form

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
-function checkname(event) {
-  var name1 =document.getElementById("name1").value;
-  var name2=document.getElementById("name2").value;
-   var names=[];
-   names.push(name1,name2);
-   alert(name1 + name2);
-    event.preventDefault();
-}
-function checkgrade(){
 
-    var question1=document.getElementById("question1").value;
-    var question1=document.getElementById("question2").value;
-    var question1=document.getElementById("question3").value;
-    var question1=document.getElementById("question4").value;
-    var question1=document.getElementById("question5").value;
-    var question1=document.getElementById("question6").value;
-    var question1=document.getElementById("question7").value;
-    var question1=document.getElementById("question8").value;
-    var question1=document.getElementById("question9").value;
-    var questions=[];
-    questions.push(question1,question2,question3,question4,question5,question6,question7,question8,question9);
-    alert(questions);
-}
+$(document).ready(function(){
+       $("input[type='button']").click(function(){
+  var quesone = parseInt($("input[name='question1']:checked").val());
+  var questwo = parseInt($("input[name='question2']:checked").val());
+  var questhree = parseInt($("input[name='question3']:checked").val());
+  var quesfour = parseInt($("input[name='question4']:checked").val());
+  var quesfive = parseInt($("input[name='question5']:checked").val());
+  var quessix = parseInt($("input[name='question6']:checked").val());
+  var quesseven = parseInt($("input[name='question7']:checked").val());
+  var queseight = parseInt($("input[name='question8']:checked").val());
+  var quesnine = parseInt($("input[name='question9']:checked").val());
+  var questen = parseInt($("input[name='question10']:checked").val());
+           }
+       });
+
+   });
